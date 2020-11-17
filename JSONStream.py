6747893fd02f1ea3bdac51408725c08d44ea67fc
toBeOfUse <mitchjacov@gmail.com>
2020-11-17 00:06:32 -0500

    organize and change the tests for JSONStream.py

--- JSONStream.py
+++ JSONStream.py
@@ -2,6 +2,7 @@ import ijson
 from pprint import pprint
 
 
+# opens file, skips past twitter's `window.YTD.yaddayadda = ` prefix
 class PrefixedJSONArray:
     def __init__(self, file):
         self.filename = file
@@ -17,6 +18,9 @@ class PrefixedJSONArray:
         self.file.close()
 
 
+# returns messageCreate and other channel event objects for either a group dm or individual dm .js file with
+# the small modification of placing the conversation ids and event names in each message instead of having
+# them outside (in the "conversationID" and "type" fields respectively)
 def message_stream(path):
     with PrefixedJSONArray(path) as json_file:
         parser = ijson.parse(json_file)
@@ -26,12 +30,13 @@ def message_stream(path):
         message = {}
         current_dict = message
 
+        event_types = ["messageCreate", "joinConversation", "participantsJoin", "participantsLeave",
+                                        "conversationNameUpdate"]
+
         for prefix, event, value in parser:
             if prefix == "item.dmConversation.conversationId":
                 conversation_id = value
-            if prefix.startswith(tuple("item.dmConversation.messages.item." + x + "." for x in
-                                       ("messageCreate", "joinConversation", "participantsJoin", "participantsLeave",
-                                        "conversationNameUpdate"))):
+            if prefix.startswith(tuple("item.dmConversation.messages.item." + x + "." for x in event_types)):
                 key = prefix.split(".")[-1]
                 in_message = True
                 message["type"] = prefix.split(".")[4]
@@ -57,6 +62,7 @@ def message_stream(path):
                 in_message = False
 
 
+# returns all the prefixes ijson finds in a file; useful for parser development
 def prefix_finder(path):
     with PrefixedJSONArray(path) as json_file:
         prefixes = set()
@@ -65,18 +71,22 @@ def prefix_finder(path):
     return prefixes
 
 
+# prints all the data ijson finds in a file, parsed; Not Recommended for large files
 def dump(path):
     with PrefixedJSONArray(path) as json_file:
         for prefix, event, value in ijson.parse(json_file):
-            print("prefix=" + prefix + ", event=" + event + ", value=" + str(value))
+            print("prefix=" + prefix + ", event=" +
+                  event + ", value=" + str(value))
 
 
+# shady test function that prints parser results for manual review
 def test(path):
-    dump(path)
-    pprint(prefix_finder(path))
     for message in message_stream(path):
         pprint(message, width=200)
 
 
 if __name__ == "__main__":
-    test("./testdata/test.js")
+    print("------------ Individual DMs Test ------------")
+    test("./testdata/individual_dms_test.js")
+    print("------------ Group DMs Test ------------")
+    test("./testdata/group_dms_test.js")
