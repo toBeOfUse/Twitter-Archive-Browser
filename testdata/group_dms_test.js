1e3c0cfe56f2ade520a0b3a9b86e918c250895d4
toBeOfUse <mitchjacov@gmail.com>
2020-11-19 11:33:31 -0500

    add group dm participants doc, release test data

---
+++ testdata/group_dms_test.js
@@ -0,0 +1,188 @@
+window.YTD.direct_messages_group.part3 = [{
+        "dmConversation": {
+            "conversationId": "convo_you_were_added_to",
+            "messages": [{
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Praesent facilisis vitae sapien a porttitor. Vivamus maximus lacinia fringilla.",
+                    "mediaUrls": [],
+                    "senderId": "31239408",
+                    "id": "700928388131790851",
+                    "createdAt": "2016-02-20T06:22:05.085Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Sed et varius nibh. Morbi sed ligula vel ante dapibus.",
+                    "mediaUrls": [],
+                    "senderId": "90420314",
+                    "id": "700928384914706435",
+                    "createdAt": "2016-02-20T06:22:04.263Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Donec sit amet turpis enim. Etiam id feugiat diam, non.",
+                    "mediaUrls": [],
+                    "senderId": "1536791610",
+                    "id": "700928326240632835",
+                    "createdAt": "2016-02-20T06:21:50.277Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Aliquam luctus fermentum fermentum. Vivamus et justo luctus, vestibulum ipsum.",
+                    "mediaUrls": [],
+                    "senderId": "813286",
+                    "id": "700928274243805187",
+                    "createdAt": "2016-02-20T06:21:37.879Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Duis molestie ligula ut sem sodales, non rutrum lacus feugiat.",
+                    "mediaUrls": [],
+                    "senderId": "813286",
+                    "id": "700928213044715523",
+                    "createdAt": "2016-02-20T06:21:23.281Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Pellentesque ante ligula, commodo et pretium eu, eleifend vitae tellus.",
+                    "mediaUrls": [],
+                    "senderId": "1536791610",
+                    "id": "700928211786473475",
+                    "createdAt": "2016-02-20T06:21:23.011Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Sunt reprehenderit dolore ea nisi elit reprehenderit.",
+                    "mediaUrls": [],
+                    "senderId": "813286",
+                    "id": "700928200306626563",
+                    "createdAt": "2016-02-20T06:21:20.263Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [{
+                        "url": "https://t.co/kuJj6jjLhJ",
+                        "expanded": "https://twitter.com/messages/media/700921094409056259",
+                        "display": "pic.twitter.com/kuJj6jjLhJ"
+                    }],
+                    "text": "Duis iaculis pretium lorem, id sodales ante accumsan nec. Maecenas. https://t.co/kuJj6jjLhJ",
+                    "mediaUrls": ["https://ton.twitter.com/dm/700921094409056259/700921094430072832/f7JooNMW.jpg"],
+                    "senderId": "90420314",
+                    "id": "700921094409056259",
+                    "createdAt": "2016-02-20T05:53:06.308Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [{
+                        "url": "https://t.co/somenonsense",
+                        "expanded": "https://youtu.be/dQw4w9WgXcQ",
+                        "display": "youtu.be/dQw4w9WgXcQ"
+                    }],
+                    "text": "https://t.co/somenonsense In incididunt velit id commodo officia deserunt ad aliquip voluptate quis id cillum.",
+                    "mediaUrls": [],
+                    "senderId": "4196983835",
+                    "id": "700920967577604099",
+                    "createdAt": "2016-02-20T05:52:35.849Z"
+                }
+            }, {
+                "participantsLeave": {
+                    "userIds": ["18393773"],
+                    "createdAt": "2016-02-20T05:35:33.497Z"
+                }
+            }, {
+                "participantsJoin": {
+                    "initiatingUserId": "3119671493",
+                    "userIds": ["5162861"],
+                    "createdAt": "2016-02-09T01:38:06.141Z"
+                }
+            }, {
+                "conversationNameUpdate": {
+                    "initiatingUserId": "16573941",
+                    "name": "yawntastic",
+                    "createdAt": "2016-02-08T18:43:50.249Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [{
+                        "senderId": "3330610905",
+                        "reactionKey": "funny",
+                        "eventId": "1320946773276291073",
+                        "createdAt": "2020-10-27T04:33:47.424Z"
+                    }],
+                    "urls": [],
+                    "text": "Proin commodo, velit id porta condimentum, ex turpis vestibulum purus.",
+                    "mediaUrls": [],
+                    "senderId": "1588486417",
+                    "id": "1320922112459186180",
+                    "createdAt": "2020-10-27T02:55:47.837Z"
+                }
+            }, {
+                "joinConversation": {
+                    "initiatingUserId": "3307595502",
+                    "participantsSnapshot": ["813286", "1536791610", "18393773", "90420314", "3330610905", "1588486417", "5162861", "4196983835", "16573941", "31239408"],
+                    "createdAt": "2015-12-20T10:51:37.176Z"
+                }
+            }]
+        }
+    },
+    {
+        "dmConversation": {
+            "conversationId": "convo_you_created",
+            "messages": [{
+                "participantsLeave": {
+                    "userIds": ["84279963"],
+                    "createdAt": "2016-02-20T05:35:33.497Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Quis cupidatat officia pariatur et cillum nostrud adipisicing adipisicing.",
+                    "mediaUrls": [],
+                    "senderId": "176566242",
+                    "id": "726719285448572932",
+                    "createdAt": "2016-05-01T09:25:54.130Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [],
+                    "text": "Quis cupidatat officia pariatur et cillum nostrud adipisicing adipisicing.",
+                    "mediaUrls": [],
+                    "senderId": "181561712",
+                    "id": "726719285448572932",
+                    "createdAt": "2016-05-01T10:25:54.130Z"
+                }
+            }, {
+                "messageCreate": {
+                    "reactions": [],
+                    "urls": [{
+                        "url": "https://t.co/SE1NPvdk85",
+                        "expanded": "https://twitter.com/messages/media/726719253861273603",
+                        "display": "pic.twitter.com/SE1NPvdk85"
+                    }],
+                    "text": " https://t.co/SE1NPvdk85",
+                    "mediaUrls": ["https://ton.twitter.com/dm/726719253861273603/726719254448476160/eH1klHSs.jpg"],
+                    "senderId": "181561712",
+                    "id": "726719253861273603",
+                    "createdAt": "2016-05-01T10:25:47.206Z"
+                }
+            }]
+        }
+    }
+]
