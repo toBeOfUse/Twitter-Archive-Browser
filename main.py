c08dbdf9bd1e37fa1b899e1fcac9d68c637e8458
toBeOfUse <mitchjacov@gmail.com>
2020-11-23 10:35:58 -0500

    reformat main.py with black

--- main.py
+++ main.py
@@ -10,37 +10,52 @@ async def main(manifest_path):
     with PrefixedJSON(manifest_path) as manifest_file:
         manifest = json.load(manifest_file)
     db_store = TwitterDataWriter(
-        manifest["userInfo"]["userName"], manifest["userInfo"]["accountId"])
-    
+        manifest["userInfo"]["userName"], manifest["userInfo"]["accountId"]
+    )
+
     def process_file(file_dict, group_dm):
         print(f"processing file {file_dict['fileName']}")
-        for message in (s:=MessageStream(Path(base_path, file_dict["fileName"]))):
+        for message in (s := MessageStream(Path(base_path, file_dict["fileName"]))) :
             db_store.add_message(message, group_dm)
             if db_store.added_messages % 1000 == 0:
-                print(f"\r{db_store.added_messages:,} total messages added; " +
-                        f"{s.percentage:.2f}% of the way through {file_dict['fileName']}", end="")
-        print(f"\r{db_store.added_messages:,} total messages added; " +
-                        f"{s.percentage:.2f}% of the way through {file_dict['fileName']}\n")
+                print(
+                    f"\r{db_store.added_messages:,} total messages added; "
+                    + f"{s.percentage:.2f}% of the way through {file_dict['fileName']}",
+                    end="",
+                )
+        print(
+            f"\r{db_store.added_messages:,} total messages added; "
+            + f"{s.percentage:.2f}% of the way through {file_dict['fileName']}\n"
+        )
 
     for individual_dm_file in manifest["dataTypes"]["directMessages"]["files"]:
         process_file(individual_dm_file, False)
 
-    print("added {:,} direct messages from {:,} users across {:,} conversations\n".format(
-        dms := db_store.added_messages, dm_users := db_store.added_users,
-        dm_convos := db_store.added_conversations)
+    print(
+        "added {:,} direct messages from {:,} users across {:,} conversations\n".format(
+            dms := db_store.added_messages,
+            dm_users := db_store.added_users,
+            dm_convos := db_store.added_conversations,
+        )
     )
-    
+
     for group_dm_file in manifest["dataTypes"]["directMessagesGroup"]["files"]:
         process_file(group_dm_file, True)
 
-    print("added {:,} group chat messages from {:,} users across {:,} conversations\n".format(
-        db_store.added_messages-dms, db_store.added_users-dm_users,
-        db_store.added_conversations-dm_convos)
+    print(
+        "added {:,} group chat messages from {:,} users across {:,} conversations\n".format(
+            db_store.added_messages - dms,
+            db_store.added_users - dm_users,
+            db_store.added_conversations - dm_convos,
+        )
     )
 
     await db_store.finalize()
 
+
 if __name__ == "__main__":
+
     async def test_run():
         await main("../data/manifest.js")
+
     IOLoop.current().run_sync(test_run)
