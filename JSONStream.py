cf79803a58d7fecc79b3263073fc977003a8b402
toBeOfUse <mitchjacov@gmail.com>
2020-11-25 04:29:26 -0500

    add docstrings to JSONStream.py

--- JSONStream.py
+++ JSONStream.py
@@ -3,12 +3,33 @@ from pprint import pprint
 from pathlib import Path
 
 
-# opens file, skips past twitter's `window.YTD.yaddayadda = ` prefix
 class PrefixedJSON:
+    """takes a .js file that assigns some json-formatted data to a global variable
+    (like the twitter archive .js files do) and skips past the assignment so that the
+    file can be used as pure json. compatible with the json and ijson modules.
+
+    Attributes:
+        filename: name/path of the file we're using.
+
+    How to use:
+        >>> with PrefixedJSON("file.js") as json_file:
+        >>>     parser = ijson.parse(json_file)
+    """
+
     def __init__(self, file):
         self.filename = file
 
     def __enter__(self):
+        """prepares a file to be read as json.
+
+        opens a file in bytes mode (for ijson compatibility/optimization purposes),
+        reads data from it until it finds a character that can act as the start of
+        some json data, then seeks backwards one byte so that that character is the
+        next one that will be read.
+
+        Returns:
+            a prepared file object.
+        """
         self.file = open(self.filename, "rb")
         byte = self.file.read(1)
         while byte != bytes("[", encoding="utf-8") and byte != bytes(
@@ -22,29 +43,66 @@ class PrefixedJSON:
         self.file.close()
 
 
-# returns messageCreate and other channel event objects for either a group dm or
-# individual dm .js file with the small modification of placing the conversation ids
-# and event names in each message instead of having them outside (in the
-# "conversationId" and "type" fields respectively)
 class MessageStream:
+    """turns twitter archive .js files containing dms into an iterable stream of
+    messages and other conversation events.
+
+    returns messageCreate and other channel event objects for either a group dm or
+    individual dm .js file with the small modification of placing the conversation ids
+    and event names in each event dict instead of having them outside (in the
+    "conversationId" and "type" fields respectively). uses the ijson module to avoid
+    loading the whole .js file into memory.
+
+    Attributes:
+        path: path to .js file that the messages are being obtained from
+        ijson_events_processed: an ijson event is raised when ijson finds some data
+        in the code; the number of processed events here serves as a progress
+        indicator. ijson events are not to be confused with conversation events like
+        messageCreate or the other ones this class yields, which contain a lot more
+        data.
+        ijson_events_total: the total number of ijson events reading this file will
+        cause.
+
+    How to use:
+        >>> for message in MessageStream("messages.js"):
+        >>>     save_in_database(message)
+    """
+
     def __init__(self, path):
+        """initializes the object and does a quick pre-count of how many ijson events
+        we will have to process.
+
+        Args:
+            path: path to the .js file this object will glean conversation events from.
+        """
         self.path = path
-        self.percentage = 0
-        # this pre-scan casues a performance pause but is the only way to get a
-        # percentage progress report that i can think of, and that makes the rest of
-        # it go faster
-        self.number_of_events = 0
+        self.ijson_events_processed = 0
+        self.ijson_events_total = 0
         with PrefixedJSON(self.path) as temp:
             for _ in ijson.parse(temp):
-                self.number_of_events += 1
+                self.ijson_events_total += 1
+
+    @property
+    def percentage(self):
+        "simplest way to see how much of the current file has been read"
+        return self.ijson_events_processed / self.ijson_events_total * 100
 
     def __iter__(self):
+        """gathers ijson events and yields dicts representing conversation events as
+        they emerge.
+
+        conversation events can be of the types messageCreate, joinConversation,
+        participantsJoin, participantsLeave, or conversationNameUpdate; this method
+        grabs each object representing one of these events, adds 'type' and
+        'conversationId' fields to them based on context from elsewhere in the json
+        data, and yields them.
+
+        """
         with PrefixedJSON(self.path) as json_file:
             conversation_id = ""
             in_message = False
             message = {}
             current_dict = message
-            handled_events = 0
 
             event_types = [
                 "messageCreate",
@@ -89,16 +147,15 @@ class MessageStream:
                             current_dict[key] = value
                 elif prefix == "item.dmConversation.messages.item" and in_message:
                     message["conversationId"] = conversation_id
-                    self.percentage = handled_events / self.number_of_events * 100
                     yield message
                     message = {}
                     current_dict = message
                     in_message = False
-                handled_events += 1
+                self.ijson_events_processed += 1
 
 
-# returns all the prefixes ijson finds in a file; useful for parser development
 def prefix_finder(path):
+    "returns all the prefixes ijson finds in a file; used for parser development"
     with PrefixedJSON(path) as json_file:
         prefixes = set()
         for p, _, _ in ijson.parse(json_file):
@@ -106,15 +163,17 @@ def prefix_finder(path):
     return prefixes
 
 
-# prints all the data ijson finds in a file, parsed; Not Recommended for large files
 def dump(path):
+    """prints all the data ijson finds in a file in ijson event form; Not Recommended
+    for large files"""
     with PrefixedJSON(path) as json_file:
         for prefix, event, value in ijson.parse(json_file):
             print("prefix=" + prefix + ", event=" + event + ", value=" + str(value))
 
 
-# shady test function that prints parser results for manual review
 def test(path):
+    """shady test function that prints the dicts that the parser outputs for a given
+    file for manual review"""
     for message in (s := MessageStream(path)) :
         pprint(message, width=200)
         print(f"percentage: {s.percentage}")
