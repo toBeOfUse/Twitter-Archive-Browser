9abe8eaa540cb5ea337b0ac8284553187d2acdac
toBeOfUse <mitchjacov@gmail.com>
2021-01-24 19:00:26 -0500

    add prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
 const path = require("path");
 
 module.exports = function (_env, argv) {
-    const isProduction = argv.mode === "production";
-    const isDevelopment = !isProduction;
-    const HTMLWebpackPlugin = require("html-webpack-plugin");
+  const isProduction = argv.mode === "production";
+  const isDevelopment = !isProduction;
+  const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-    return {
-        devtool: isDevelopment && "cheap-module-source-map",
-        entry: "./frontend/index.js",
-        stats: {
-            assets: false,
-            modules: false
+  return {
+    devtool: isDevelopment && "cheap-module-source-map",
+    entry: "./frontend/index.js",
+    stats: {
+      assets: false,
+      modules: false,
+    },
+    output: {
+      path: path.resolve(__dirname, "frontend"),
+      filename: "assets/js/[name].[contenthash:8].js",
+      publicPath: "/",
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: "./frontend/index.html",
+        filename: "assets/html/index.html",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? "production" : "development",
+            },
+          },
         },
-        output: {
-            path: path.resolve(__dirname, "frontend"),
-            filename: "assets/js/[name].[contenthash:8].js",
-            publicPath: "/"
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
-        plugins: [
-            new HTMLWebpackPlugin({
-                template: "./frontend/index.html",
-                filename: "assets/html/index.html"
-            })
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            cacheDirectory: true,
-                            cacheCompression: false,
-                            envName: isProduction ? "production" : "development"
-                        }
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: ["style-loader", "css-loader"]
-                }
-            ]
-        }
-    };
+      ],
+    },
+  };
 };
