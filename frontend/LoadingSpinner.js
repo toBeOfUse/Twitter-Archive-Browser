3094d7f1a26cde50696a19cc4959c053f7753134
toBeOfUse <mitchjacov@gmail.com>
2021-01-28 21:01:27 -0500

    loading spinner

---
+++ frontend/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LoadingSpinner() {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
