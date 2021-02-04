40ecb9d4ef44061975ad9e7182e11a36d1808ac7
toBeOfUse <mitchjacov@gmail.com>
2021-02-04 01:34:34 -0500

    add name_updates_chronological_idx

--- SQLScripts/indexes.sql
+++ SQLScripts/indexes.sql
@@ -21,6 +21,8 @@ create index media_by_message_idx on media (message);
 
 create index links_by_message_idx on links (message);
 
+create index name_updates_chronological_idx on name_updates (update_time);
+
 create index name_updates_convo_chronological_idx on name_updates (conversation, update_time);
 
 create index participation_start_idx on participants (start_time);
